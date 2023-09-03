6c63da6aba9583a5e695f97831adcd3486dd2a7c
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-09-03 11:43:34 -0700

    created test route for requireAuth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,6 @@
 // Necessary imports
 const express = require('express')
-const { setTokenCookie } = require('../../utils/auth')
-const { restoreUser } = require('../../utils/auth.js')
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth')
 const { User } = require('../../db/models')
 
 // Instantiate a router
@@ -29,5 +28,11 @@ router.use(restoreUser)
 router.get('/restore-user', (req, res) => {
     return res.json(req.user)
 })
+
+// Test route for requireAuth, if there is no session user, the route will return an error, otherwise it will return the session user's information
+router.get('/require-auth', requireAuth, (req, res) => {
+    return res.json(req.user)
+})
+
 
 module.exports = router
