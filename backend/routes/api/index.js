e0fa5fb10abd44467b5cdc837ea654931e27d5ea
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-09-03 11:24:51 -0700

    successfully tested setTokenCookie route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,7 @@
 // Necessary imports
 const express = require('express')
+const { setTokenCookie } = require('../../utils/auth')
+const { User } = require('../../db/models')
 
 // Instantiate a router
 const router = express.Router()
@@ -7,6 +9,17 @@ const router = express.Router()
 // Test route
 router.post('/test', (req, res) => {
     res.json({requestBody: req.body})
+})
+
+// Test route for setTokenCookie, see if there is a token cookie set in your nrowser's DevTools
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'demouser'
+        }
+    })
+    setTokenCookie(res, user)
+    return res.json({ user: user })
 })
 
 module.exports = router
