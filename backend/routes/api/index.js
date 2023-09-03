97c22c7bf678e1da24722cbf7b9e348265a8b3a8
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-09-03 11:30:48 -0700

    ready to test restoreUser endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
 // Necessary imports
 const express = require('express')
 const { setTokenCookie } = require('../../utils/auth')
+const { restoreUser } = require('../../utils/auth.js')
 const { User } = require('../../db/models')
 
 // Instantiate a router
@@ -11,7 +12,7 @@ router.post('/test', (req, res) => {
     res.json({requestBody: req.body})
 })
 
-// Test route for setTokenCookie, see if there is a token cookie set in your nrowser's DevTools
+// Test route for setTokenCookie, see if there is a token cookie set in your browser's DevTools
 router.get('/set-token-cookie', async (_req, res) => {
     const user = await User.findOne({
         where: {
@@ -20,6 +21,13 @@ router.get('/set-token-cookie', async (_req, res) => {
     })
     setTokenCookie(res, user)
     return res.json({ user: user })
+})
+
+// Test route for restoreUser
+router.use(restoreUser)
+
+router.get('/restore-user', (req, res) => {
+    return res.json(req.user)
 })
 
 module.exports = router
