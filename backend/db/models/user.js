4b49018b21524405717ff4d0dd51400be8c92e68
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-09-03 04:12:47 -0700

    added model scopes to User model

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -44,6 +44,21 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
+      }
+    },
+    scopes: {
+      currentUser: {
+        attributes: {
+          exclude: ['hashedPassword']
+        }
+      },
+      loginUser: {
+        attributes: {}
+      }
+    }
   });
   return User;
 };
